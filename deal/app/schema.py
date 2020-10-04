3124ddf70103415e2f24db5e2561b13d43219765
sudheesh85 <sudheesh85@gmail.com>
2020-10-03 23:02:32 -0500

    modified

--- deal/app/schema.py
+++ deal/app/schema.py
@@ -3,6 +3,7 @@ from graphene import relay,ObjectType, Schema,Mutation
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django import DjangoObjectType
 from app.models import User
+from .userid_gen import uid,otp
 
 class UserType(DjangoObjectType):
     class Meta:
@@ -14,25 +15,53 @@ class UserInput(graphene.InputObjectType):
     name=graphene.String()
     mobile=graphene.String()
     status=graphene.String()
+    otp=graphene.String()
+    otp_exp_time=graphene.DateTime()
     #price=graphene.Int()
 class AddUser(graphene.Mutation):
     class Arguments:
+        #id=graphene.ID(required=True)
         input=UserInput(required=True)   
     user=graphene.Field(UserType)
 
     @staticmethod
-    def mutate(root, info, input=None):
-        user = User(
-            #name=input.name, 
-            mobile=input.mobile, 
-            status=input.status,
-            #price=input.price,
-            )
+    def mutate(root, info,input=None):
+        print(input.mobile)
+        try:
+            user = User.objects.get(mobile=input.mobile)
+        #user=info.context.user
+            print("user:",user)
+            if user.mobile == input.mobile:
+                user.name=input.name
+        except:
+            user = User(
+                #name=input.name, 
+                mobile=input.mobile, 
+                status=input.status,
+                #price=input.price,
+                )
         user.save()
         return AddUser(user=user)
-
+class ResendOTP(graphene.Mutation):
+    class Arguments:
+        input=UserInput(required=True)   
+    user=graphene.Field(UserType)
+    @staticmethod
+    def mutate(root, info,input=None):
+        try:
+            user = User.objects.get(mobile=input.mobile)
+        #user=info.context.user
+            print("user:",user)
+            if user.mobile == input.mobile:
+                user.otp=otp.get_otp()
+                user.otp_exp_time=otp.get_exp_time()
+        except:
+            pass
+        user.save()
+        return ResendOTP(user=user)
 class Mutation(ObjectType):
     add_user = AddUser.Field()
+    Resend_otp=ResendOTP.Field()
 
 class Query(ObjectType):
     user = graphene.Field(UserType, pk=graphene.Int())
