8a5434fdca36b153de15943a31e4c93e56cec408
sudheesh85 <sudheesh85@gmail.com>
2020-11-05 19:52:54 -0600

    modified

--- deal/deal/urls.py
+++ deal/deal/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', GraphQLView.as_view(graphiql=True)),
+    path('', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
